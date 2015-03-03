114a2b9820ace9ed6ad638cf145c0c4b86048911
Matthew Hailwood <me@matthewhailwood.co.nz>
2015-03-04 10:39:59 +1300

    Make ExtraPageFields move metadata to new tab; Updated readme

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 ## Composer ##
 Run the following to add this module as a requirement and install it via composer.
 
-
 ```
 #!bash
 
@@ -17,3 +16,39 @@ Several of these helpers are disabled by default but can be enabled in the confi
 * Silverstripe 3.1+
 * php5.4+
 * php5-intl Package 
+
+#Extension Overview#
+
+##DataListExtension##
+Adds a new *ListColumn* function to a DataList to allow implode() style functionality of the list items e.g. `$list->ListColumn('ID', ' |')` 
+would result in something like "1 |18 |19 |24".
+
+##ExtraPageFieldsExtension##
+ - Moves the metadata fields to a new tab
+ - Adds a new "Meta Title" field
+ 
+##FooterMenuExtension##
+ - Adds a new *ShowInFooter* option to the page settings
+ - Adds a new `FooterPages()` function to the SiteTree to return only pages that have this checked
+
+#Template Providers Overview#
+
+##HelpersTemplateProvider
+ - Adds a new `Repeat($times)` function to the templates to return a loopable list (kind of like a for loop)
+ - Adds a new `Dump($obj)` function to the templates to allow easy dumping of any template variable
+ 
+#Extendable Classes Overview#
+ 
+##OwnerPermissionedDataObject##
+ - Allows data objects to inherit the access permissions from their attached relation parent
+ - Relation parent defaults to `Page()`
+ 
+#Page Types Overview#
+ 
+##ChildlessPage##
+ - This class by itself does nothing 
+ - The idea behind this class is that in your template for the main navigation a "ChildlessPage" should never be rendered as a dropdown
+ 
+##NavHolderPage##
+ - If accessed directly this page will return a 404
+ - The idea behind this class is that it should act as a label for your site navigation and not be a page itself.
--- code/ExtraPageFieldsExtension.php
+++ code/ExtraPageFieldsExtension.php
@@ -7,23 +7,34 @@
 class ExtraPageFieldsExtension extends SiteTreeExtension {
 
 	private static $db = array(
-			'SubTitle' => 'Text',
-			'MetaTitle' => 'Text'
+		'SubTitle'  => 'Text',
+		'MetaTitle' => 'Text'
 	);
 
-	public function updateCMSFields(FieldList $fields){
+	public function updateCMSFields(FieldList $fields) {
 
 		//Add secondary heading - H2
-		$fields->addFieldToTab('Root.Main', new TextField('SubTitle', 'Secondary Heading'), 'MenuTitle');
-		
+		$fields->addFieldToTab('Root.Main', TextField::create('SubTitle', 'Secondary Heading'), 'MenuTitle');
+
 		//change Page Name label to Primary Heading - H1
 		$fields->removeFieldFromTab('Root.Main', 'Page name');
-		$fields->addFieldToTab('Root.Main', new TextField('Title', 'Primary Heading'), 'SubTitle');
-		 
+		$fields->addFieldToTab('Root.Main', TextField::create('Title', 'Primary Heading'), 'SubTitle');
+
+		//Move meta fields to their own tab
+
+		/** @var ToggleCompositeField $metaDataChildren */
+		$metaDataChildren = $fields->fieldByName('Root.Main.Metadata');
+		$children = array_merge([$metaTitle = TextField::create('MetaTitle')], $metaDataChildren->getChildren()->toArray());
+		$fields->removeFieldFromTab('Root.Main', 'Metadata');
+		$fields->addFieldToTab('Root', Tab::create('Metadata'), 'Content');
+
 		//Add META Title tag to METADATA
-		$fields->addFieldToTab('Root.Main.Metadata', new TextField('MetaTitle', $title = 'Meta Title'), $above = 'MetaDescription');
-	
+		$fields->addFieldsToTab('Root.Metadata', $children);
+
+		$metaTitle->setDescription('Displayed as the tab/window name; Also displayed in search engine result listings as the page title.<br />
+									Falls back to the Primary Heading field if not provided.');
+
 	}
-	
+
 }
 
