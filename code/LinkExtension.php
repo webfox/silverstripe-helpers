1be28bf3e03388e8bfa0e041debf69aa1b2e49d3
Otis Wright <otis@beardedweb.com>
2015-11-10 11:29:42 +1300

    Updating Link Extensions
    Updating Link Extensions to allow content authors to edit links

--- code/LinkExtension.php
+++ code/LinkExtension.php
@@ -53,4 +53,20 @@ class LinkExtension extends DataExtension {
 
         return join(' ', [$this->owner->getTargetAttr(), $this->getDownloadAttribute(), $this->getTitleAttribute()]);
     }
+    
+    public function canView($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canEdit($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canDelete($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canCreate($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
 }
