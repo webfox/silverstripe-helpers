9645ae3edf31bf14cb4efa6dc51b27f3fa493f4b
Lester <lestercomia@gmail.com>
2016-02-23 13:23:25 +1300

    metadescription

--- code/ExtraPageFieldsExtension.php
+++ code/ExtraPageFieldsExtension.php
@@ -4,52 +4,77 @@
  * Class ExtraPageFieldsExtension
  *
  */
-class ExtraPageFieldsExtension extends SiteTreeExtension {
+class ExtraPageFieldsExtension extends SiteTreeExtension
+{
 
-	private static $db = array(
-		'SubTitle'  => 'Text',
-		'MetaTitle' => 'Text',
-		'MenuTarget' => 'Varchar(255)'
-	);
+    private $metaDescriptionLength = 156;
 
-	public function updateCMSFields(FieldList $fields) {
+    private static $db = array(
+        'SubTitle'   => 'Text',
+        'MetaTitle'  => 'Text',
+        'MenuTarget' => 'Varchar(255)'
+    );
 
-		//change Page Name label to Primary Heading - H1 - Only if the title hasn't already been changed
-		/** @var TextField $titleField */
-		$titleField = $fields->dataFieldByName('Title');
-		if ($titleField->Title() == 'Page Name') {
-			$fields->renameField('Title', 'Primary Heading');
-		}
+    public function updateCMSFields(FieldList $fields)
+    {
 
-		//Add secondary heading - H2
-		$fields->insertAfter(TextField::create('SubTitle', 'Secondary Heading'), 'Title');
+        //change Page Name label to Primary Heading - H1 - Only if the title hasn't already been changed
+        /** @var TextField $titleField */
+        $titleField = $fields->dataFieldByName('Title');
+        if ($titleField->Title() == 'Page name') {
+            $fields->renameField('Title', 'Primary Heading');
+        }
 
-		//Move meta fields to their own tab
+        //Add secondary heading - H2
+        $fields->insertAfter(TextField::create('SubTitle', 'Secondary Heading'), 'Title');
 
-		/** @var ToggleCompositeField $metaDataChildren */
-		$metaDataChildren = $fields->fieldByName('Root.Main.Metadata');
-		$children = array_merge([$metaTitle = TextField::create('MetaTitle')], $metaDataChildren->getChildren()->toArray());
-		$fields->removeFieldFromTab('Root.Main', 'Metadata');
-		$fields->addFieldToTab('Root', Tab::create('Metadata'), 'Content');
+        //Move meta fields to their own tab
 
-		//Add META Title tag to METADATA
-		$fields->addFieldsToTab('Root.Metadata', $children);
+        /** @var ToggleCompositeField $metaDataChildren */
+        $metaDataChildren = $fields->fieldByName('Root.Main.Metadata');
+//		ddd($metaDataChildren->fieldByName('MetaDescription'));
+        $children = array_merge([$metaTitle = TextField::create('MetaTitle')], $metaDataChildren->getChildren()->toArray());
+        $fields->removeFieldFromTab('Root.Main', 'Metadata');
+        $fields->addFieldToTab('Root', Tab::create('Metadata'), 'Content');
 
-		$metaTitle->setDescription('Displayed as the tab/window name; Also displayed in search engine result listings as the page title.<br />
+        //Add META Title tag to METADATA
+        $metaDataChildren->fieldByName('MetaDescription')->setAttribute('maxlength', $this->metaDescriptionLength);
+        $fields->addFieldsToTab('Root.Metadata', $children);
+
+        $metaTitle->setDescription('Displayed as the tab/window name; Also displayed in search engine result listings as the page title.<br />
 									Falls back to the Primary Heading field if not provided.');
 
-	}
+    }
 
-	public function updateSettingsFields(FieldList $fields) {
-		//quick links option
-		$fields->addFieldToTab("Root.Settings", new DropdownField('MenuTarget', 'Open page in', [
-			'' => 'Current Tab (Browser default)',
-			'_blank' => 'New Tab'
-		]));
-	}
+    public function updateSettingsFields(FieldList $fields)
+    {
+        //quick links option
+        $fields->addFieldToTab("Root.Settings", new DropdownField('MenuTarget', 'Open page in', [
+            ''       => 'Current Tab (Browser default)',
+            '_blank' => 'New Tab'
+        ]));
+    }
 
-	Public function MenuTarget(){
-		return empty($this->owner->MenuTarget) ? '' : "target=\"{$this->owner->MenuTarget}\"";
-	}
+    Public function MenuTarget()
+    {
+        return empty($this->owner->MenuTarget) ? '' : "target=\"{$this->owner->MenuTarget}\"";
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+
+        if (strlen($this->owner->MetaDescription) > $this->metaDescriptionLength) {
+            /** @var Text $value */
+            $value                        = $this->owner->dbObject('MetaDescription');
+            $value                        = $value->LimitCharacters($this->metaDescriptionLength);
+            $this->owner->MetaDescription = $value;
+        };
+
+
+        //here
+    }
 }
+
 
