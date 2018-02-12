ae596317ce04a5304c07a81dd95f662dac786787
Reece Alexander <reece.alexander@gmail.com>
2018-02-13 07:35:09 +1000

    Update SS_RelativeAssetsResponse.php

--- code/SS_RelativeAssetsResponse.php
+++ code/SS_RelativeAssetsResponse.php
@@ -7,7 +7,6 @@
  */
 class SS_RelativeAssetsResponseExtension extends Extension
 {
-
     function onBeforeInit()
     {
         if (is_a($this->owner, 'Controller')) {
@@ -21,16 +20,12 @@ class SS_RelativeAssetsResponse extends SS_HTTPResponse
 {
     public function setBody($body)
     {
-        $body = $body ? (string)$body : $body;
+        $body = (string)$body;
 
         $body = str_replace('"/assets/', '"assets/', $body);
         $body = str_replace('"assets/', '"/assets/', $body);
-        
-        json_decode($body);
-        $bodyIsJson = json_last_error() === JSON_ERROR_NONE;
 
-
-        if (!$bodyIsJson && class_exists('zz\Html\HTMLMinify')) {
+        if (!json_decode($body) && class_exists('zz\Html\HTMLMinify')) {
             $this->body = zz\Html\HTMLMinify::minify($body, [
                 'doctype' => zz\Html\HTMLMinify::DOCTYPE_HTML5,
                 'optimizationLevel' => zz\Html\HTMLMinify::OPTIMIZATION_ADVANCED
